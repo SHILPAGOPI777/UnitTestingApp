3a5e3a146d2453e083a1c5b8284186edca00df4c
Shilpa Gopi <Sgopi3284@conestogac.on.ca>
2024-06-12 16:20:08 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace NUnitDemonstrationproj
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+        // SHILPA GOPI 8893284 UNIT TEST 
+    }
+    public static class Triangle
+    {
+        public static string ValidTriangle(int firstAngle, int secondAngle, int
+        thirdAngle)
+        {
+            string result;
+            if ((firstAngle + secondAngle + thirdAngle) == 180)
+            {
+                result = "The triangle is valid.";
+            }
+            else
+            {
+                result = "The triangle is NOT valid.";
+            }
+            return result;
+        }
+    }
+
+}
+
+
